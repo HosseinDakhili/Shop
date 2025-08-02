7a99a9f4a3ff094d5ce8792b15fc0ef67ff463d8
HosseinDakhili <hosseindakhili94@gmail.com>
2025-08-03 00:43:14 +0330

    feat: implement search functionality in Navbar with dynamic results display

--- client/src/Components/Navbar/index.jsx
+++ client/src/Components/Navbar/index.jsx
@@ -10,15 +10,33 @@ import {
   Button,
 } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../Store/Slices/AuthSlice";
+import { useState } from "react";
+import fetchData from "../../Utils/fetchData";
 
 export default function Navbar() {
   const cartLength = useSelector((state) => state.cart.items)?.length;
-  const { token } = useSelector((state) => state.auth);
+  const { jwt } = useSelector((state) => state.auth);
+  const [searchValue, setSearchValue] = useState("");
+  const [searchResults,setSearchResults] = useState()
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    setSearchValue(e.target.value);
+    if(e.target.value.length<2)
+      return;
+    const response = await fetchData('search',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(searchValue)
+    }) 
+    setSearchResults(response.data)
+  };
   return (
     <AppBar position="static" color="default" elevation={3}>
       <Toolbar sx={{ justifyContent: "space-between", flexWrap: "wrap" }}>
@@ -38,15 +56,24 @@ export default function Navbar() {
           >
             Products
           </Button>
-          {token ? (
-            <Button
-              component={Link}
-              to="/profile"
-              color="primary"
-              variant="text"
-            >
-              Profile
-            </Button>
+          {jwt ? (
+            <>
+              <Button
+                component={Link}
+                to="/profile"
+                color="primary"
+                variant="text"
+              >
+                Profile
+              </Button>
+              <Button
+                variant="content"
+                sx={{ backgroundColor: "red", color: "white" }}
+                onClick={() => dispatch(logout())}
+              >
+                Logout
+              </Button>
+            </>
           ) : (
             <Button component={Link} to="/auth" color="primary" variant="text">
               Login/Register
@@ -58,20 +85,39 @@ export default function Navbar() {
           direction="row"
           spacing={2}
           alignItems="center"
+          position={"relative"}
           mt={{ xs: 2, sm: 0 }}
         >
           <TextField
             size="small"
             placeholder="Search..."
+            value={searchValue}
+            onChange={handleSearch}
             variant="outlined"
             sx={{ backgroundColor: "white", borderRadius: 1 }}
           />
+          <Stack
+            sx={{
+              position: "absolute",
+              boxShadow:'0 2px 8px rgba(0,0,0,0.2)',
+              borderRadius:'0 0 10px 10px',
+              width: "100%",
+              height: searchValue ? "500px" : "0",
+              overflowY: "scroll",
+              transition: "height 0.3s ease",
+              backgroundColor:'white',
+              top:'100%',
+              zIndex:1000
+            }}
+          >
+            {!searchResults&&<Typography textAlign={'center'} my={'20px'} px={'5%'} >Enter 3 character </Typography>}
+          </Stack>
+
           <IconButton color="primary" component={Link} to="/cart">
             <Badge badgeContent={cartLength} color="error">
               <ShoppingCartIcon />
             </Badge>
           </IconButton>
-          <Button variant="content" sx={{backgroundColor:'red',color:'white'}} onClick={()=>dispatch(logout())}>Logout</Button>
         </Stack>
       </Toolbar>
     </AppBar>
